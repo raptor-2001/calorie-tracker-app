52a645e0d382975d2d4e94bf09e344b04bcc24ae
raptor-2001 <pratham007yadav@gmail.com>
2022-10-29 20:11:36 +0530

    app completed

--- app.js
+++ app.js
@@ -1,4 +1,73 @@
 // Storage Controller
+const StorageCtrl = (function(){
+
+  // Public methods
+  return{
+    storeItem: function(item){
+      let items;
+
+      if(localStorage.getItem('items') === null){
+          items = [];
+
+          // Push new item
+          items.push(item);
+
+          // Convert it into the string
+          localStorage.setItem('items',JSON.stringify(items));
+
+      }else{
+
+        // Get what is already in ls
+        items = JSON.parse(localStorage.getItem('items'));
+
+        // Push new item
+        items.push(item);
+
+        // Convert it into the string
+        localStorage.setItem('items',JSON.stringify(items));
+      }
+    },
+    getItemsFromStorage: function(){
+
+      let items;
+      if(localStorage.getItem('items') === null){
+        items = [];
+      }else{
+        items = JSON.parse(localStorage.getItem('items'))
+      }
+
+      return items;
+
+    },
+
+    updateItemStorage: function(updatedItem){
+      let items = JSON.parse(localStorage.getItem('items'));
+
+      items.forEach(function(item,index){
+          if(updatedItem.id === item.id){
+            items.splice(index,1,updatedItem);
+          }
+      });
+      localStorage.setItem('items',JSON.stringify(items));
+    },
+    deleteItemFromStorage: function(id){
+
+      let items = JSON.parse(localStorage.getItem('items'));
+
+      items.forEach(function(item,index){
+        if(item.id === id){
+          items.splice(index,1)
+        }
+      });
+
+      localStorage.setItem('items',JSON.stringify(items));
+
+    },
+    clearAllItemsFromStorage: function(){
+      localStorage.clear();
+    }
+  }
+})();
 
 
 // Item Controller
@@ -12,13 +81,9 @@ const ItemCtrl = (function(){
 
   // Data Structure / State
   const data = {
-    items: [
-      // {id: 0, name: 'Steak Dinner', calories: 1200},
-      // {id: 1, name: 'Cookie', calories: 400},
-      // {id: 2, name: 'Eggs', calories: 300}
-    ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
-    totalCalories: 0
+    totalCalories: 0 
   }
 
   // Public methods
@@ -249,7 +314,7 @@ const UICtrl = (function(){
 
 
 // App Controller
-const App = (function(ItemCtrl, UICtrl){
+const App = (function(ItemCtrl, UICtrl,StorageCtrl){
   // Load event listeners
   const loadEventListeners = function(){
     // Get UI selectors
@@ -295,8 +360,12 @@ const App = (function(ItemCtrl, UICtrl){
 
       // Get total calories
       const totalCalories = ItemCtrl.getTotalCalories();
+
       // Add total calories to UI
       UICtrl.showTotalCalories(totalCalories);
+
+      // Add new item in storage
+      StorageCtrl.storeItem(newItem );
 
       // Clear fields
       UICtrl.clearInput();
@@ -346,6 +415,9 @@ const App = (function(ItemCtrl, UICtrl){
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
+    // Update local storage
+    StorageCtrl.updateItemStorage(updatedItem);
+
     UICtrl.clearEditState();
 
     e.preventDefault();
@@ -365,8 +437,12 @@ const App = (function(ItemCtrl, UICtrl){
 
     // Get total calories
     const totalCalories = ItemCtrl.getTotalCalories();
+
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
+
+    // Delete an item form the id
+    StorageCtrl.deleteItemFromStorage(currentItem.id);
 
     UICtrl.clearEditState();
 
@@ -383,10 +459,14 @@ const App = (function(ItemCtrl, UICtrl){
 
     // Get total calories
     const totalCalories = ItemCtrl.getTotalCalories();
+
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
 
-     // Remove form UI
+    // Clear All from the Storage
+    StorageCtrl.clearAllItemsFromStorage();
+
+    // Remove form UI
      UICtrl.clearItems();
 
     //  hide the list
@@ -421,7 +501,7 @@ const App = (function(ItemCtrl, UICtrl){
     }
   }
   
-})(ItemCtrl, UICtrl);
+})(ItemCtrl, UICtrl,StorageCtrl);
 
 // Initialize App
 App.init();
